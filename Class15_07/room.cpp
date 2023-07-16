f53539ab7f5e41c0d53de4f741f61e2b30cc5d43
Louis Oosthuizen <ljdragon1997@gmail.com>
2023-07-16 14:22:08 +0200

    Inclusion_C++Implementation
    Inclusion of working C++ implementations for Room and Sphere projects.

--- Class15_07/room.cpp
+++ Class15_07/room.cpp
@@ -1,57 +1,138 @@
 #include <GL/glut.h>
 
- 
+GLfloat rotation = 0.0;
 
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void init();
+void display();
+void reshape(int width, int height);
+void draw_room();
+void draw_cube();
 
-    // Set up viewing transformation
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-
-    // Enable lighting and depth testing
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_COLOR_MATERIAL);
 
-    // Set up light position and properties
-    GLfloat light_position[] = { 1, 1, 1, 0 };
-    GLfloat light_color[] = { 1, 1, 1, 1 };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
+    // Enable and set up the first light source (directional light)
+    glEnable(GL_LIGHT0);
+    GLfloat light0_direction[] = { 1.0f, 1.0f, 1.0f, 0.0f }; // Directional light direction
+    GLfloat light0_color[] = { 1.0f, 1.0f, 1.0f, 1.0f }; // White light color
+    glLightfv(GL_LIGHT0, GL_POSITION, light0_direction);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light0_color);
 
-    // Set material properties for the sphere
-    GLfloat ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-    GLfloat diffuse[] = { 0.6, 0.6, 0.6, 1.0 };
-    GLfloat specular[] = { 0.8, 0.8, 0.8, 1.0 };
-    GLfloat shininess = 64.0;
-    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+    // Enable and set up the second light source (point light)
+    glEnable(GL_LIGHT1);
+    GLfloat light1_position[] = { 2.0f, 2.0f, 2.0f, 1.0f }; // Point light position
+    GLfloat light1_color[] = { 0.0f, 1.0f, 0.0f, 1.0f }; // Green light color
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_color);
 
-    // Draw a sphere
-    glutSolidSphere(1.0, 50, 50);
-
-    glFlush();
+    // Enable and set up the third light source (spotlight)
+    glEnable(GL_LIGHT2);
+    GLfloat light2_position[] = { 0.0f, 3.0f, 0.0f, 1.0f }; // Spotlight position
+    GLfloat light2_direction[] = { 0.0f, -1.0f, 0.0f }; // Spotlight direction
+    GLfloat light2_color[] = { 1.0f, 0.0f, 0.0f, 1.0f }; // Red light color
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+    glLightfv(GL_LIGHT2, GL_SPOT_DIRECTION, light2_direction);
+    glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_color);
+    glLightf(GL_LIGHT2, GL_SPOT_CUTOFF, 30.0f); // Spotlight cutoff angle
+    glLightf(GL_LIGHT2, GL_SPOT_EXPONENT, 2.0f); // Spotlight exponent
 }
 
- 
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 25.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); // Adjust the viewing perspective
 
-int main(int argc, char** argv)
-{
+    glRotatef(30.0f, 1.0f, 0.0f, 0.0f); // Rotate around x-axis
+    glRotatef(rotation, 0.0f, 1.0f, 0.0f); // Rotate around y-axis
+
+    draw_room();
+
+    glFlush();
+    glutSwapBuffers();
+
+    rotation += 0.03f;
+}
+
+void reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, static_cast<float>(width) / height, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void draw_room() {
+    // Draw the walls
+    glPushMatrix();
+    glTranslatef(0.0f, 2.5f, 0.0f);
+    glScalef(5.0f, 5.0f, 5.0f); // Increase the scaling factor
+    draw_cube();
+    glPopMatrix();
+}
+
+void draw_cube() {
+    glBegin(GL_QUADS);
+
+    // Back wall
+    glColor3f(0.5f, 0.5f, 0.5f); // Gray color
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    // Left wall - Glossy
+    glColor3f(1.0f, 1.0f, 1.0f); // White color
+    GLfloat diffuse_color[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+    GLfloat specular_color[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat shininess[] = { 100.0f };
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_color);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_color);
+    glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+
+    // Right wall - Matte
+    glColor3f(1.0f, 1.0f, 1.0f); // Gray color
+    GLfloat matte_diffuse_color[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, matte_diffuse_color);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_color);
+    glMaterialfv(GL_FRONT, GL_SHININESS, shininess);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+
+    // Top wall
+    glColor3f(0.8f, 0.8f, 0.8f); // Gray color
+    glVertex3f(-1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, -1.0f);
+    glVertex3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-1.0f, 1.0f, 1.0f);
+
+    // Bottom wall
+    glColor3f(0.5f, 0.5f, 0.5f); // Gray color
+    glVertex3f(-1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, -1.0f);
+    glVertex3f(1.0f, -1.0f, 1.0f);
+    glVertex3f(-1.0f, -1.0f, 1.0f);
+
+    glEnd();
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("Metallic Sphere");
-
-    glClearColor(0, 0, 0, 1);
-
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("OpenGL Room");
+    init();
     glutDisplayFunc(display);
-
+    glutReshapeFunc(reshape);
+    glutIdleFunc(display);
     glutMainLoop();
-
     return 0;
 }
--- Class15_07/sphere.cpp
+++ Class15_07/sphere.cpp
@@ -1,57 +1,59 @@
 #include <GL/glut.h>
 
- 
+GLfloat rotation = 0.0;
 
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Set up viewing transformation
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
-
-    // Enable lighting and depth testing
+void init() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
-    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
 
-    // Set up light position and properties
-    GLfloat light_position[] = { 1, 1, 1, 0 };
-    GLfloat light_color[] = { 1, 1, 1, 1 };
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    // Light source against the sphere as follows:
+    GLfloat light_pos[] = { 1.0, 1.0, 1.0, 0.0 };
+    GLfloat light_color[] = { 1.0, 1.0, 1.0, 1.0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_color);
 
-    // Set material properties for the sphere
-    GLfloat ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-    GLfloat diffuse[] = { 0.6, 0.6, 0.6, 1.0 };
-    GLfloat specular[] = { 0.8, 0.8, 0.8, 1.0 };
-    GLfloat shininess = 64.0;
-    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, specular);
-    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
-
-    // Draw a sphere
-    glutSolidSphere(1.0, 50, 50);
-
-    glFlush();
+    // Set the metallic effects of the sphere
+    GLfloat sphere_color[] = { 0.8, 0.8, 0.8, 1.0 };
+    GLfloat sphere_shininess = 100.0;
+    glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, sphere_color);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, sphere_color);
+    glMaterialf(GL_FRONT, GL_SHININESS, sphere_shininess);
 }
 
- 
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glRotatef(rotation, 1.0, 1.0, 1.0);
+    glutSolidSphere(1.0, 50, 50);
+    glFlush();
+    glutSwapBuffers();
 
-int main(int argc, char** argv)
-{
+    // Set the sphere to rotate
+    rotation += 0.5;
+}
+
+void reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0, static_cast<float>(width) / height, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(500, 500);
-    glutCreateWindow("Metallic Sphere");
-
-    glClearColor(0, 0, 0, 1);
-
+    glutCreateWindow("OpenGL Sphere");
+    init();
     glutDisplayFunc(display);
-
+    glutReshapeFunc(reshape);
+    glutIdleFunc(display);
     glutMainLoop();
-    
     return 0;
 }
